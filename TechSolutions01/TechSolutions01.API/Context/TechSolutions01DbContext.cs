462308d248732aca5fd7f78959d6230cdf79698b
philanip <philani.p@hotmail.com>
2023-11-13 23:10:40 +0200

    added DbContext for TechSolutions01

---
+++ TechSolutions01/TechSolutions01.API/Context/TechSolutions01DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechSolutions01.Models;
+
+namespace TechSolutions01.API.Context
+{
+    public class TechSolutions01DbContext : DbContext
+    {
+        public TechSolutions01DbContext(DbContextOptions<TechSolutions01DbContext> options)
+            :base(options)
+        {            
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
