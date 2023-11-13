95d328f1191f60d9b00f3c9a252199f9bd1515a9
philanip <philani.p@hotmail.com>
2023-11-14 00:09:05 +0200

    added Customer controller with action methods

--- TechSolutions01/TechSolutions01.API/Program.cs
+++ TechSolutions01/TechSolutions01.API/Program.cs
@@ -1,9 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TechSolutions01.API.Context;
+using TechSolutions01.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<TechSolutions01DbContext>(opt =>
+    opt.UseSqlServer("TechSolutions01WebContext"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
